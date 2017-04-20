74e6e6a19ce38288415f2348464f895075c46e82
srgupta5328 <srgupta5328@gmail.com>
2017-04-20 15:24:07 -0400

    Added the exchange class endits

--- src/logic/Exchange.java
+++ src/logic/Exchange.java
@@ -1,5 +1,33 @@
 package logic;
 
-public class Exchange {
+import Controller.Transaction;
+
+public class Exchange extends Transaction {
+	 ProductModel itemBack;
+	   ProductModel itemTo;
+	        
+	    
+	    public void Exchanging (ProductModel sitemBack, ProductModel sitemTo) {
+	        itemBack = sitemBack;
+	        itemTo = sitemTo;
+	        
+	        
+	    }
+	        
+
+	    @Override
+	    public void run() {
+	        
+	       for (int i = 0; i < Inventory.sizd(); i++) {
+	            synchronized(Inventory) {
+	            if (Inventory.get(i).iD == itemTo.iD) {
+	                Inventory.add(itemBack);
+	                Inventory.remove(i);
+	            }
+
+	            } 
+
+	        }
+	    }
 
 }
