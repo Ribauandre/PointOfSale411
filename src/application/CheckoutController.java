9947665b339c029b1c6ea6d752a124fd5da7b7f9
Steven Rohan <smr5735@gmail.com>
2017-05-01 20:28:09 -0400

    den swag
    comments/methods waiting for Return XML and GUI (commented out)

--- src/application/CheckoutController.java
+++ src/application/CheckoutController.java
@@ -54,11 +54,12 @@ public class CheckoutController implements Initializable{
 		Main.showOrder();
 		}
 	
-	
+	/*
 	@FXML
 	public void returnClick(){ // return 
 		Main.showReturn(); 
 	}
+	*/
 	
 	private void handleReturnItem(){
 		for(int j = 0; j< ShoppingCart.cart.size(); j++){
--- src/application/Main.java
+++ src/application/Main.java
@@ -74,11 +74,12 @@ public class Main extends Application {
          }
      }
     
+    /*
     public static void showReturn() { // return
     	FXMLLoader loader = new FXMLLoader();
         loader.setLocation(Main.class.getResource("CheckoutView.fxml")); 
     }
-    
+    */
     
     public static void showCart() { // reveals the items the customer has added
         try {
